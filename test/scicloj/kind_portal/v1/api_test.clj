3b3f66bcc984f530a6e282d1d65a53ce09d63c18
Timothy Pratley <timothypratley@gmail.com>
2023-10-09 12:24:06 -0700

    avoid using new `update-vals` core fn
    As a library, we shouldn't force users to use a new Clojure version.
    Seeing it is easy to avoid using `update-vals`, just use another
    approach.

--- test/scicloj/kind_portal/v1/api_test.clj
+++ test/scicloj/kind_portal/v1/api_test.clj
@@ -1,9 +1,14 @@
 (ns scicloj.kind-portal.v1.api_test
-  (:require  [scicloj.kind-portal.v1.api :as kind-portal]
-             [scicloj.kindly.v4.kind :as kind]
-             [scicloj.kindly-advice.v1.api :as kindly-advice]
-             [tablecloth.api :as tc]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [scicloj.kind-portal.v1.api :as kind-portal]
+            [scicloj.kindly.v4.kind :as kind]))
 
-(kind-portal/kindly-submit-context
- {:form '(kind/hiccup
-          [:h1 "a"])})
+(deftest submit-context-test
+  (kind-portal/kindly-submit-context
+    {:form `(kind/hiccup [:h1 "a"])}))
+
+(deftest preparation-test
+  (is (= "^#:kindly{:kind :kind/map} {^{:kindly/kind :kind/hiccup, :portal.viewer/default :portal.viewer/hiccup} [:div \"A\"] ^{:kindly/kind :kind/hiccup, :portal.viewer/default :portal.viewer/hiccup} [:div \"B\"], ^#:portal.viewer{:default :portal.viewer/hiccup} [:div nil [:portal.viewer/inspector :c]] {^#:portal.viewer{:default :portal.viewer/hiccup} [:div nil [:portal.viewer/inspector :d]] ^{:kindly/kind :kind/hiccup, :portal.viewer/default :portal.viewer/hiccup} [:div \"E\"]}}"
+         (binding [*print-meta* true]
+           (pr-str (kind-portal/prepare {:value (kind/map {(kind/hiccup [:div "A"]) (kind/hiccup [:div "B"])
+                                                           :c                       {:d (kind/hiccup [:div "E"])}})}))))))
